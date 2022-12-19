c315995ac77cd436cf251b5ba3c6f990f1293f3b
MikiWagnr <miguelmontilla96@gmail.com>
2022-12-19 00:58:42 -0500

    Fizzbuzz

---
+++ fizzbuzz/Wagner_Miguel_Fizz_Buzz.js
@@ -0,0 +1,12 @@
+
+for (var num = 1; num < 101; num++) {
+    if (num % 5 == 0 && num % 3 == 0) {
+        console.log('FizzBuzz')
+    } else if (num % 5 == 0) {
+        console.log('Buzz')
+    } else if (num % 3 == 0) {
+        console.log('Fizz')
+    } else {
+        console.log(num)
+    }
+}
