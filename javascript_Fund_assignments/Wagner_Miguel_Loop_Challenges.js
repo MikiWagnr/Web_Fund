6d7b9001739b954cb78d87d752be1dbfa5b47fa8
MikiWagnr <miguelmontilla96@gmail.com>
2022-12-19 02:36:24 -0500

    button-clicker

---
+++ javascript_Fund_assignments/Wagner_Miguel_Loop_Challenges.js
@@ -0,0 +1,11 @@
+//1)
+/*for(var num = 1; num <=20; num+=2){
+    console.log(num)
+}*/
+//2)
+/*for(var num = 100; num >=0; num--){
+    if(num % 3 ==0){
+        console.log(num)
+    }
+}*/
+//3)
