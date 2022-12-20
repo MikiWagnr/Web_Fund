f45f64c30c67d859ba9afe2475a1847b34b6dde3
MikiWagnr <miguelmontilla96@gmail.com>
2022-12-20 18:28:33 -0500

    liking likes js updatex with comments

--- liking_likes/part2/script.js
+++ liking_likes/part2/script.js
@@ -1,4 +1,4 @@
 //increases likes
 function increaseLikes(id){
-    document.querySelector(id).innerText++
+    document.querySelector(id).innerText++//the selector findsthe id parameter, targets the inner text and then adds by 1
 }
